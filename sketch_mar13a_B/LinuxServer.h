08fd033a5f2676d8c5ff40621750ffd577420961
Brian Bowles <113460376+SpiritsFlare@users.noreply.github.com>
2025-06-18 10:17:56 +0200

    Delete sketch_mar13a_B directory

--- sketch_mar13a_B/LinuxServer.h
+++
@@ -1,8 +0,0 @@
-#ifndef LINUXSERVER_H
-#define LINUXSERVER_H
-
-// Server details for  Linux Server with Hostafrica
-const char* LINUX_SERVER = "160.119.248.229"; // Server IP address
-const int LINUX_PORT = 8080;  // Server port
-
-#endif
